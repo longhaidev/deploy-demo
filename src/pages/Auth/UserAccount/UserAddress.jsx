597667e38f0ebfbaabfdfae0ff099e8e1823b562
HLOEY <default_crt@yahoo.com>
2024-08-06 00:35:46 +0700

    bug: fix not found module

--- src/pages/Auth/UserAccount/UserAddress.jsx
+++ src/pages/Auth/UserAccount/UserAddress.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import DefaultButton from "components/Button/DefaultButton";
-import UserAddressCard from "components/UserAddressCard/UserAddressCard";
+import UserAddressCard from "components/User/UserAddressCard";
 import ModalAddAddress from "components/ModalBox/ModalAddAddress";
 
 export default function UserAddress() {
