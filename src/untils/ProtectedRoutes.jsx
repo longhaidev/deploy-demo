c62469ab0a42ecaeb6e94c0cd694dbf267b67e4d
HLOEY <default_crt@yahoo.com>
2024-08-06 00:29:56 +0700

    feat: update deploy

--- src/untils/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-export default function ProtectedRoutes() {
-  const isAuth = useSelector((state) => state.user.isAuthenticated);
-
-  return isAuth ? <Outlet></Outlet> : <Navigate to="/login"></Navigate>;
-}
